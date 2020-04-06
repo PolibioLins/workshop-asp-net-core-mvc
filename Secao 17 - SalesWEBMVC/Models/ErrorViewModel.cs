b2d19b76d86a9ab3096ba1eba75ccd4a2a1f22de
Polibio Lins <polibio.l.costa@gmail.com>
2020-04-06 09:26:56 -0300

    Add project files.

---
+++ Secao 17 - SalesWEBMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Secao_17___SalesWEBMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
