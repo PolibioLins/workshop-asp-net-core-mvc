3a4b5e98b7ee4b697e296bd95e98099f90a458d5
Polibio Lins <polibio.l.costa@gmail.com>
2020-04-08 13:02:34 -0300

    App locale, number and date formatting

--- Secao 17 - SalesWEBMVC/Startup.cs
+++ Secao 17 - SalesWEBMVC/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Secao17SalesWEBMVC.Data;
 using Secao_17___SalesWEBMVC.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 
 namespace Secao_17___SalesWEBMVC
@@ -51,6 +53,17 @@ namespace Secao_17___SalesWEBMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
